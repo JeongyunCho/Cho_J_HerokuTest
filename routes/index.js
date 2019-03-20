7a22bf94d97b938e55aec8455bf3b6489ee11e38
unknown <j_cho6@fanshaweonline.ca>
2019-03-20 01:24:25 -0400

    page created, node finished

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//! Require data.js
 const data = require('../data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Your first Express App', message: 'Express is actually pretty easy once you figure a few things out' });
+  res.render('index', data);
 });
 
-// get a dynamic user via the route parameter
-router.get('/:user', function(req, res, next) {
-  console.log('user:', data[req.params.user]);
-  res.render('userpage', data[req.params.user]);
+/* GET Learn More page. */
+router.get('/learnmore', function (req, res, next) {
+  res.render('learnmore', data);
 });
 
 module.exports = router;
+
